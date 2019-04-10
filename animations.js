546e688fc6047ab77e626f2a432a7e4e2a9ddada
filmonabr <39414083+filmonabr@users.noreply.github.com>
2019-04-10 04:04:35 -0500

    html and js files modified

--- animations.js
+++ animations.js
@@ -15,7 +15,7 @@
 // PUT YOUR CODE IN THERE.  In myanimation.js, it is legal for you to modify
 // the variables that have been declared in this file.
 
-var BLANK = "blank";
+var BLANK = "";
 
 var CUSTOM = "";   // you could change the value of this variable in your code
 
@@ -640,10 +640,11 @@ var DIVE = "  o\n" +
 
 // can also access the animations as an associative array / hash map,
 // such as ANIMATIONS["Bike"] or ANIMATIONS["Dive"]
-var ANIMATIONS = [];
+var ANIMATIONS = new Array();//[];
 ANIMATIONS["Blank"] = ANIMATIONS["blank"] = ANIMATIONS["BLANK"] = "";
 ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = CUSTOM;   // you could change the value of this in your code
 ANIMATIONS["Exercise"] = ANIMATIONS["exercise"] = ANIMATIONS["EXERCISE"] = EXERCISE;
 ANIMATIONS["Juggler"] = ANIMATIONS["juggler"] = ANIMATIONS["JUGGLER"] = JUGGLER;
 ANIMATIONS["Bike"] = ANIMATIONS["bike"] = ANIMATIONS["BIKE"] = BIKE;
 ANIMATIONS["Dive"] = ANIMATIONS["dive"] = ANIMATIONS["DIVE"] = DIVE;
+
